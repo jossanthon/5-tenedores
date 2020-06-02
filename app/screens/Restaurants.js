9bd5010a6f1050192741cd6e4da85aa829e71d7e
joss <jos.anthon77@gmail.com>
2020-06-02 15:22:26 -0400

    se ha modificado el texto de restaurnate

--- app/screens/Restaurants.js
+++ app/screens/Restaurants.js
@@ -4,7 +4,7 @@ import {View, Text} from "react-native";
 export default function Restaurants(){
     return (
         <View>
-            <Text> restaurants..</Text>
+            <Text> Restaurants..</Text>
         </View>
     );
 };
